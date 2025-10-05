ae086900c215fe8541e5e5e0e2713ea6b5d085f0
Meena Atai <meenaatai2005@gmail.com>
2025-10-05 05:22:19 -0400

    premiere version

--- src/scripts.js
+++ src/scripts.js
@@ -2,3 +2,11 @@
 import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Collapse, Ripple });
+
+//Modal
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
